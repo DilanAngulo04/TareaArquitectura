09934bf84f909ab58f7a274738845920689839db
Dilan Angulo <dilan0400ar@gmail.com>
2018-09-20 15:39:02 -0600

    Creación de las clases del proyecto

---
+++ src/cr/ac/ucr/if400/tarea1/domain/Flotante.java
@@ -0,0 +1,12 @@
+package cr.ac.ucr.if400.tarea1.domain;
+
+public class Flotante {
+    short valor;
+
+    public Flotante() {
+        this.valor = 0;
+    }
+    
+    
+    
+}
---
+++ src/cr/ac/ucr/if400/tarea1/main/Principal.java
@@ -0,0 +1,10 @@
+
+package cr.ac.ucr.if400.tarea1.main;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
